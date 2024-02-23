5ed55ef568494e259354dfe71111551e97cee43c
Vladislav Ivanov <vladislav2097@mail.ru>
2024-02-23 15:02:32 +0200

    Update authorization mechanism to use JWT tokens

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { TypegooseModule } from 'nestjs-typegoose'
+import { getJWTConfig } from '../config/jwt.config'
 import { UserModel } from '../user/user.model'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
-  controllers: [AuthController],
-  imports: [
-    TypegooseModule.forFeature([
-      {
-        typegooseClass: UserModel,
-        schemaOptions: {
-          collection: 'User',
-        },
-      },
-    ]),
-    ConfigModule,
-  ],
-  providers: [AuthService],
+	controllers: [AuthController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
+	],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
