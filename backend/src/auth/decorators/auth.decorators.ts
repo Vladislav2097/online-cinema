2edd7999742e7721dbe69bf9710ed5d79d1a1d2c
Vladislav Ivanov <vladislav2097@mail.ru>
2024-02-26 15:39:34 +0200

    Add token retrieval, User and Auth decorators

---
+++ backend/src/auth/decorators/auth.decorators.ts
@@ -0,0 +1,11 @@
+import { applyDecorators, UseGuards } from '@nestjs/common'
+import type { TypeRole } from '../auth.interface'
+import { OnlyAdminGuard } from '../guards/admin.guards'
+import { JwtAuthGuard } from '../guards/jwt.guards'
+
+export const Auth = (role: TypeRole = 'user') =>
+	applyDecorators(
+		role === 'admin'
+			? UseGuards(JwtAuthGuard, OnlyAdminGuard)
+			: UseGuards(JwtAuthGuard)
+	)
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from './user.model'
+import { UserService } from './user.service'
+import { UserController } from './user.controller'
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController]
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+		ConfigModule,
+	],
+	controllers: [UserController],
+	providers: [UserService],
 })
 export class UserModule {}
